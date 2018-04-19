11ccad373d6d86fab35df83993e0ff66cab8abdf
wangshujing <wangshujing@uccc.cc>
2018-04-19 16:06:29 +0800

    test

--- src/Testscan.java
+++ src/Testscan.java
@@ -1,22 +1,44 @@
+
 /**
  * Created by Administrator on 2018/4/19.
  *
  *测试Scanner类的使用，如何接受键盘的输入
  *
  */
-public class TestScanner {
-    public static void main(String[] args)
-    {
-        System.out.println("请输入一个数");
+import java.util.Scanner;
+public class Testscan {
+    public static void test01(){
+        System.out.println("请输入一个人整数:");
         System.out.println();
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         if(a%2==0)
         {
-            System.out.println("这是一个偶数");
+            System.out.println("你输入的数是偶数");
         }else
         {
-            System.out.println("这是一个奇数");
+            System.out.println("你输入的数为奇数");
         }
     }
+    public static void test02(){
+        System.out.println("请输入年份：");
+        System.out.println();
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        if(year%4==0&&year%100!=0||year%400==0)
+        {
+            System.out.println("您输入的年分为闰年");
+
+        } else {
+            System.out.println("您输入的年份为平年");
+        }
+
+    }
+    public static void main(String[] args)
+    {
+        test01();
+        test02();
+
+    }
+
 }
--- src/Testwhile.java
+++ src/Testwhile.java
@@ -1,7 +1,31 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Administrator on 2018/4/19.
  */
 public class Testwhile {
+   /*
+    public static void main(String[] args)
+    {
+        int x = 10;
+        while(x<20)
+        {
+            System.out.println("value of x:" +x);
+            x++;
+            System.out.print("\n");
+        }
+    }
+    */
+    public static void main(String[] args)
+    {
+        int[] numbers = {10, 20, 30, 40, 50};
+        for(int x : numbers){
+            System.out.print(x);
+            System.out.print(",");
+        }
+        System.out.print("\n");
+        String[] names ={"James", "Lsrry", "Tom", "Lacy"};
+        for(String name : names){
+            System.out.print(name);
+            System.out.print(",");
+        }
+    }
 }
